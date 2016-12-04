ca2ef435b8dca156531140e2403b9553cbf70dc1
Philip Schwarz <pschwarz@expedia.com>
2016-12-04 18:17:59 +0000

    initial

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "scala-akka-rabbitmq-client"
+
+version := "1.0"
+
+scalaVersion := "2.10.0"
+
+resolvers += "The New Motion Public Repo" at "http://nexus.thenewmotion.com/content/groups/public/"
+
+libraryDependencies += "com.thenewmotion.akka" %% "akka-rabbitmq" % "2.3"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,53 @@
+
+package com.thenewmotion.akka.rabbitmq
+package examples
+
+import akka.actor.{ ActorRef, ActorSystem }
+import concurrent.Future
+import concurrent.ExecutionContext.Implicits.global
+
+/**
+  * @author Yaroslav Klymko
+  */
+object Main extends App {
+  implicit val system = ActorSystem()
+  val factory = new ConnectionFactory()
+  val connection = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
+  val exchange = "amq.fanout"
+
+  def setupPublisher(channel: Channel, self: ActorRef) {
+    val queue = channel.queueDeclare().getQueue
+    channel.queueBind(queue, exchange, "")
+  }
+  connection ! CreateChannel(ChannelActor.props(setupPublisher), Some("publisher"))
+
+  def setupSubscriber(channel: Channel, self: ActorRef) {
+    val queue = channel.queueDeclare().getQueue
+    channel.queueBind(queue, exchange, "")
+    val consumer = new DefaultConsumer(channel) {
+      override def handleDelivery(consumerTag: String, envelope: Envelope, properties: BasicProperties, body: Array[Byte]) {
+        println("received: " + fromBytes(body))
+      }
+    }
+    channel.basicConsume(queue, true, consumer)
+  }
+  connection ! CreateChannel(ChannelActor.props(setupSubscriber), Some("subscriber"))
+
+  Future {
+    def loop(n: Long) {
+      val publisher = system.actorSelection("/user/rabbitmq/publisher")
+
+      def publish(channel: Channel) {
+        channel.basicPublish(exchange, "", null, toBytes(n))
+      }
+      publisher ! ChannelMessage(publish, dropIfNoChannel = false)
+
+      Thread.sleep(1000)
+      loop(n + 1)
+    }
+    loop(0)
+  }
+
+  def fromBytes(x: Array[Byte]) = new String(x, "UTF-8")
+  def toBytes(x: Long) = x.toString.getBytes("UTF-8")
+}
